48693f022869592736e1df6e649266b59ebec2b9
MAGIkCODES <justiceodoyi@gmail.com>
2024-01-13 00:48:13 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import sideBar from '@/components/sideBar.vue'
+  import ImageGallery from './components/ImageGallery.vue';
+  import{ ref } from 'vue'
+
+  const isWithColor = ref(true)
+  const onSelectOption = (isColor) => {
+    isWithColor.value = isColor
+  }
+</script>
+
+<template>
+  <v-app>
+   <sideBar @selectOption="onSelectOption" />
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor" />
+    </v-main>
+  </v-app>
+</template>
+
+
---
+++ src/components/sideBar.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+const Emits = defineEmits(['selectOption'])
+const selectOption = (isColor) => {
+    Emits("selectOption", isColor)
+}
+</script>
+
+<template>
+   <v-navigation-drawer
+     expand-on-hover
+     rail
+   >
+    <v-list>
+        <v-list-item
+        prepend-avatar="https://res.cloudinary.com/dqrykh6qy/image/upload/v1676592415/samples/backgrounds/48311435-5371-4784-AF61-048556EB7A40_xjksk5.jpg"
+        title="Justice Odoyi"
+        subtitle="justiceodoyi@gmail.com"
+        ></v-list-item>
+    </v-list>
+
+    <v-divider />
+
+    <v-list density="compact" nav>
+        <v-list-item
+            prepend-icon="mdi-image"
+            title="Color Gallery"
+            value="color"
+            @click="selectOption(true)"
+        />
+        <v-list-item
+            prepend-icon="mdi-dialpad"
+            title="Grayspace Gallery"
+            value="color"
+            @click="selectOption(false)"
+        />
+    </v-list>
+   </v-navigation-drawer>
+</template>
